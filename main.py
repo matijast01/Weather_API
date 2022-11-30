54b08736d6ef98cd8c3a7dc5da441c9689a3d2ea
Matijast <matijastipcevic01@yahoo.com>
2022-11-30 18:33:15 +0000

    Add url endpoint for yearly and all data

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def specific_data(station, date):
     station_id = station.zfill(6)
     filename = f"data_small/TG_STAID{station_id}.txt"
 
@@ -24,6 +24,36 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/annual/<station>/<year>")
+def year_data(station, year):
+    station_id = station.zfill(6)
+    filename = f"data_small/TG_STAID{station_id}.txt"
+
+    df = pd.read_csv(filename, skiprows=20)
+    df["TG"] = df["   TG"] / 10
+    df = df[["    DATE", "TG"]]
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    for date_temp in result:
+        date_temp["    DATE"] = date_temp["    DATE"][:4] \
+                                + "-" + date_temp["    DATE"][4:6] \
+                                + "-" + date_temp["    DATE"][6:]
+    return result
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    station_id = station.zfill(6)
+    filename = f"data_small/TG_STAID{station_id}.txt"
+
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["TG"] = df["   TG"] / 10
+    df = df[["    DATE", "TG"]]
+    result = df.to_dict(orient="records")
+
+    return result
 
 
 if __name__ == "__main__":
